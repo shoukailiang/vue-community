47484e738a796c69c69fff093a31e6080563e007
shoukailiang <shoukailiang@qq.com>
2021-02-20 03:13:33 +0800

    查看他人个人中心页面

--- pages/user/_id.vue
+++ pages/user/_id.vue
@@ -4,12 +4,7 @@
     <el-row class="main-top">
       <div class="avatar">
         <el-tooltip content="点击上传头像" effect="light">
-          <el-upload
-            accept="image/*"
-            action=""
-            :show-file-list="false"
-            :http-request="uploadMainImg"
-          >
+          <el-upload accept="image/*" action="" :show-file-list="false">
             <el-avatar :size="120" :src="this.userInfo.imageUrl">{{
               userInfo.nickName
             }}</el-avatar>
@@ -37,34 +32,13 @@
             @fetch-data="findUserArticleList"
           />
         </el-tab-pane>
-        <el-tab-pane label="私密文章" name="nopublic">
-          <article-list
-            name="nopublic"
-            :page="query"
-            :listData="articleList"
-            @fetch-data="findUserArticleList"
-          />
-        </el-tab-pane>
+
         <el-tab-pane label="我的提问" name="question">
           <question-list
             name="question"
             :page="query"
             :listData="questionList"
             @fetch-data="findUserQuestionList"
-          />
-        </el-tab-pane>
-        <el-tab-pane label="修改个人资料" name="user">
-          <user-edit
-            :loading="loading"
-            :formData="userInfo"
-            @submitForm="submitUserForm"
-          />
-        </el-tab-pane>
-        <el-tab-pane label="修改密码" name="password">
-          <user-password
-            :loading="loading"
-            :formData="passwordData"
-            @submitForm="submitPasswordForm"
           />
         </el-tab-pane>
       </el-tabs>
@@ -74,27 +48,24 @@
 <script>
 import ArticleList from "@/components/article/List";
 import QuestionList from "@/components/question/List";
-import UserEdit from "@/components/user/Edit";
-import UserPassword from "@/components/user/Password";
 
 export default {
-  // 引用中间件，拦截路由请求，判断是否已经身份认证
-  middleware: "auth",
-  components: { QuestionList, ArticleList, UserEdit, UserPassword },
-
+  components: { QuestionList, ArticleList },
   data() {
     return {
       loading: false,
       userInfo: {
         imageUrl: "",
       },
-      passwordData: {
-        // 修改密码表单
-        userId: this.$store.state.userInfo && this.$store.state.userInfo.uid,
-      },
       questionList: [], // 提问列表
+      userId: "",
     };
   },
+  validate({ params }) {
+    // 必须是number类型
+    return /^\d+$/.test(params.id);
+  },
+
   methods: {
     // 查询我的提问列表（
     async findUserQuestionList(paneName, current) {
@@ -143,76 +114,11 @@ export default {
           break;
       }
     },
-
-    // 上传头像
-    uploadMainImg(file) {
-      // 封装上传头像表单数据
-      const data = new FormData();
-      data.append("file", file.file);
-      this.$uploadImg(data)
-        .then((response) => {
-          if (response.code === 20000) {
-            // 删除原图片
-            this.deleteImg();
-            // 回显上传后的图片
-            this.userInfo.imageUrl = response.data;
-            // 将用户头像url更新到数据库中
-            this.$updateUserInfo(this.userInfo)
-          }
-        })
-        .catch(() => {
-          this.$message.error("上传头像失败");
-        });
-    },
-
-    // 删除头像, 上传成功后删除原来的头像
-    deleteImg() {
-      if (this.userInfo.imageUrl) {
-        // 如果有原图地址，则删除它，
-        this.$deleteImg(this.userInfo.imageUrl);
-      }
-    },
-
-    // 提交修改个人资料
-    async submitUserForm() {
-      this.loading = true;
-      // 提交修改的个人信息
-      this.userInfo;
-      const { code, message } = await this.$updateUserInfo(this.userInfo);
-      if (code === 20000) {
-        this.$message.success("修改成功");
-      } else {
-        this.$message.error(message);
-      }
-      this.loading = false;
-    },
-
-    // 修改密码
-    async submitPasswordForm() {
-      // 确认中
-      this.loading = true;
-      // 封装数据
-      this.passwordData.userId =
-        this.$store.state.userInfo && this.$store.state.userInfo.uid;
-      const { code, message } = await this.$updatePassword(this.passwordData);
-
-      if (code === 20000) {
-        // 清空修改密码表单
-        this.passwordData = {};
-        // 跳转到登录页
-        this.$store.dispatch("LoginPage");
-      } else {
-        // 修改失败
-        this.$message.error(message);
-      }
-
-      this.loading = false;
-    },
   },
 
-  async asyncData({ app, store }) {
+  async asyncData({ app, store, params }) {
     // 1. 查询用户信息
-    const userId = store.state.userInfo && store.state.userInfo.uid;
+    const userId = params.id;
     const { data: userInfo } = await app.$getUserInfo(userId);
 
     // 2. 查询公开文章列表
